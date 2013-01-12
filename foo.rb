34f7e1cf06a3bbd41ceb9b5c529fb5589dc7411a
Nick Dimiduk <ndimiduk@gmail.com>
2013-01-12 14:18:49 -0800

    add dummy formula

---
+++ foo.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class Foo < Formula
+  url 'http://ftp.gnu.org/gnu/coreutils/coreutils-8.20.tar.xz'
+  homepage 'http://www.n10k.com'
+  md5 '3d69af8f561fce512538a9fe85f147ff'
+end
