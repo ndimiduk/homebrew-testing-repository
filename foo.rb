bcee1314c882f36fc6e2e848d6a559e6cb95158f
Nick Dimiduk <ndimiduk@gmail.com>
2013-01-12 14:34:00 -0800

    foo

--- foo.rb
+++ foo.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Foo < Formula
-  url 'http://ftp.gnu.org/gnu/coreutils/coreutils-8.20.tar.xz'
+  url 'http://ftp.gnu.org/gnu/coreutils/coreutils-5.2.0.tar.gz'
   homepage 'http://www.n10k.com'
-  md5 '3d69af8f561fce512538a9fe85f147ff'
+  md5 '235f64f2f4f9a1be7fc652c0fb6e631a'
 end
