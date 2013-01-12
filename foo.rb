fbd15c37526d4ecd9f68127400d7b513e347d036
Nick Dimiduk <ndimiduk@gmail.com>
2013-01-12 14:34:52 -0800

    foo

--- foo.rb
+++ foo.rb
@@ -4,4 +4,7 @@ class Foo < Formula
   url 'http://ftp.gnu.org/gnu/coreutils/coreutils-5.2.0.tar.gz'
   homepage 'http://www.n10k.com'
   md5 '235f64f2f4f9a1be7fc652c0fb6e631a'
+
+  def install
+  end
 end
